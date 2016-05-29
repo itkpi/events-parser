509141204ddcd982a67ef5ed77a5d4082a6c61b8
Maxym Vlasov <MaxymVlasov@users.noreply.github.com>
2016-05-30 01:43:11 +0300

    Fix name log files

--- log.js
+++ log.js
@@ -4,7 +4,10 @@ const fs = require('fs-extra')
 
 exports._log_ = (log, name) => {
   let d = new Date()
-  if (!name) name = d.getMonth()
+  if (!name) {
+    name = d.getMonth() + 1
+    if (name < 10) name = `0${name}`
+  }
 
   fs.appendFile(`./logs/${d.getFullYear()}_${name}`, `${d}: ${log}\n`, (err) => {
     if (err) throw err

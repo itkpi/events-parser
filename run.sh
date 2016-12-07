43c67450d6a0ff340e435c1c29e11af5223b1ea8
Roman Rader <antigluk@gmail.com>
2016-12-07 22:46:35 +0200

    run.sh

---
+++ run.sh
@@ -0,0 +1,7 @@
+
+export EMAIL='PARSER@IT.KPI'
+export HOSTNAME_URL='events-admin'
+export HOSTNAME_PATH='/api/v1/suggested_events'
+export HOSTNAME_PORT='5000'
+
+timeout -t 600 node main.js || echo "KILLED BY TIMEOUT"

46dff9ed3e46de372cd0cff4bcf308be1fd56df0
Roman Rader <antigluk@gmail.com>
2016-12-07 23:39:15 +0200

    statsd

--- data/dataIO.js
+++ data/dataIO.js
@@ -14,6 +14,18 @@ const giveConfig = require('../src.js').config
 const dataIO = {}
 module.exports = dataIO
 
+
+
+const url = require('url');
+const parsedStatsdUrl = url.parse(process.env.STATSD_URL, true, true);
+const StatsD = require('node-statsd');
+const statsClient = new StatsD(
+        {
+          host: parsedStatsdUrl.hostname,
+          port: parsedStatsdUrl.port,
+        }
+      );
+
 const firstEvent = 0
 
 /**
@@ -142,6 +154,8 @@ dataIO.sendtoAPI = (title, agenda, social, place, regUrl, imgUrl, whenStart, whe
     'submitter_email': process.env.EMAIL
   })
 
+  statsClient.increment('eventsparser.sendToAPI.sends');
+
   const options = {
     'hostname': process.env.HOSTNAME_URL,
     'port': process.env.HOSTNAME_PORT,
@@ -161,7 +175,10 @@ dataIO.sendtoAPI = (title, agenda, social, place, regUrl, imgUrl, whenStart, whe
       res.on('end', () => { _log_('No more data in response. \n') })
     }
   })
-  req.on('error', (e) => { _log_(`problem with request: ${e.message}`) })
+  req.on('error', (e) => {
+   _log_(`problem with request: ${e.message}`);
+   statsClient.increment('eventsparser.sendToAPI.errors');
+  })
 
   req.write(body)
   req.end()
